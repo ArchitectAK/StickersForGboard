6bddac2d27e79d62ccb266259a657c8e07f9c0bf
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-11-21 13:54:32 +0800

    started coding for stickers

---
+++ app/src/main/java/com/cogitator/stickersforgboard/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.cogitator.stickersforgboard
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author Ankit Kumar on 21/11/2018
+ */
+class MainActivity :AppCompatActivity(){
+
+}
