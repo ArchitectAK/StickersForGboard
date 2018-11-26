9cd40cb007dbc74381e7439db9c922115aced895
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-11-26 17:56:41 +0800

    fixing

--- app/src/main/java/com/cogitator/stickersforgboard/StickerIndexingUtil.kt
+++ app/src/main/java/com/cogitator/stickersforgboard/StickerIndexingUtil.kt
@@ -1,40 +1,30 @@
 package com.cogitator.stickersforgboard
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Color
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.appindexing.FirebaseAppIndex
+import com.google.firebase.appindexing.FirebaseAppIndexingInvalidArgumentException
+import com.google.firebase.appindexing.Indexable
+import com.google.firebase.appindexing.builders.Indexables
+import com.google.firebase.appindexing.builders.StickerBuilder
 import java.io.File
-import java.io.FileOutputStream
 import java.io.IOException
-import java.util.*
+import java.util.ArrayList
 
 /**
  * @author Ankit Kumar on 26/11/2018
  */
 object StickerIndexingUtil {
-    private const val STICKER_FILENAME_PATTERN = "sticker%s.png"
-    private val CONTENT_URI_ROOT = String.format("content://%s/", StickerProvider::class.java.name)
-    private const val STICKER_URL_PATTERN = "mystickers://sticker/%s"
-    private const val STICKER_PACK_URL_PATTERN = "mystickers://sticker/pack/%s"
-    private const val STICKER_PACK_NAME = "Local Content Pack"
-    private const val TAG = "AppIndexingUtil"
-    private const val FAILED_TO_CLEAR_STICKERS = "Failed to clear stickers"
-    private const val FAILED_TO_INSTALL_STICKERS = "Failed to install stickers"
+    private val STICKER_URL_PATTERN = "mystickers://sticker/%s"
+    private val STICKER_PACK_URL_PATTERN = "mystickers://sticker/pack/%s"
+    private val STICKER_PACK_NAME = "Local Content Pack"
+    private val TAG = "AppIndexingUtil"
+    val FAILED_TO_INSTALL_STICKERS = "Failed to install stickers"
 
-    fun clearStickers(context: Context, firebaseAppIndex: FirebaseAppIndex) {
-        val task = firebaseAppIndex.removeAll()
-
-        task.addOnSuccessListener {
-            Toast.makeText(context, "Successfully cleared stickers", Toast.LENGTH_SHORT).show()
-        }
-        task.addOnFailureListener { e ->
-            Log.w(TAG, FAILED_TO_CLEAR_STICKERS, e)
-            Toast.makeText(context, FAILED_TO_CLEAR_STICKERS, Toast.LENGTH_SHORT).show()
-        }
-    }
-
+    //this just adds the stickers
     fun setStickers(context: Context, firebaseAppIndex: FirebaseAppIndex) {
         try {
             val stickers = getIndexableStickers(context)
@@ -43,7 +33,9 @@ object StickerIndexingUtil {
             val indexables = ArrayList(stickers)
             indexables.add(stickerPack)
 
-            val task = firebaseAppIndex.update(*indexables.toTypedArray())
+            val task = firebaseAppIndex.update(
+                *indexables.toTypedArray()
+            )
 
             task.addOnSuccessListener {
                 Toast.makeText(context, "Successfully added stickers", Toast.LENGTH_SHORT)
@@ -57,11 +49,69 @@ object StickerIndexingUtil {
             }
         } catch (e: IOException) {
             Log.e(TAG, "Unable to set stickers", e)
+            Toast.makeText(context, FAILED_TO_INSTALL_STICKERS, Toast.LENGTH_SHORT)
+                .show()
         } catch (e: FirebaseAppIndexingInvalidArgumentException) {
             Log.e(TAG, "Unable to set stickers", e)
+            Toast.makeText(context, FAILED_TO_INSTALL_STICKERS, Toast.LENGTH_SHORT).show()
         }
+
     }
 
+    @Throws(IOException::class)
+    private fun getIndexableStickers(context: Context): List<Indexable> {
+        val indexableStickers = ArrayList<Indexable>()
+        val stickerBuilders = getStickerBuilders(context)
+
+        for (stickerBuilder in stickerBuilders) {
+            stickerBuilder
+                .setIsPartOf(
+                    Indexables.stickerPackBuilder()
+                        .setName(STICKER_PACK_NAME)
+                )
+            indexableStickers.add(stickerBuilder.build())
+        }
+
+        return indexableStickers
+    }
+
+    // this builds the INDIVIDUAL stickers
+    @Throws(IOException::class)
+    private fun getStickerBuilders(context: Context): List<StickerBuilder> {
+        val stickerBuilders = ArrayList<StickerBuilder>()
+
+        val stickersDir = File(context.filesDir, "stickers")
+
+        if (!stickersDir.exists() && !stickersDir.mkdirs()) {
+            throw IOException("Stickers directory does not exist")
+        }
+
+        for (i in 0 until HomeActivity.stickers.length) {
+            val stickerFilename = getStickerFilename(i)
+            val keywords = HomeActivity.stickers[i].getKeywords()
+
+            val stickerBuilder = Indexables.stickerBuilder()
+                .setName(stickerFilename)
+                // Firebase App Indexing unique key that must match an intent-filter
+                // see: Support links to your app content section
+                // (e.g. mystickers://sticker/0)
+                .setUrl(String.format(STICKER_URL_PATTERN, i))
+                // http url or content uri that resolves to the sticker
+                // (e.g. http://www.google.com/sticker.png or content://some/path/0)
+                .setImage(HomeActivity.stickers[i].getImageUrl())
+                .setDescription("content description")
+                .setIsPartOf(
+                    Indexables.stickerPackBuilder()
+                        .setName(STICKER_PACK_NAME)
+                )
+                .setKeywords(*keywords)
+            stickerBuilders.add(stickerBuilder)
+        }
+
+        return stickerBuilders
+    }
+
+    //this makes the sticker PACK
     @Throws(IOException::class, FirebaseAppIndexingInvalidArgumentException::class)
     private fun getIndexableStickerPack(context: Context): Indexable {
         val stickerBuilders = getStickerBuilders(context)
@@ -73,8 +123,9 @@ object StickerIndexingUtil {
 
         // Use the last sticker for category image for the sticker pack.
         val lastIndex = stickerBuilders.size - 1
-        val stickerName = getStickerFilename(lastIndex)
-        val imageUrl = getStickerUrl(stickerName)
+
+        // user sticker method here
+        val imageUrl = HomeActivity.stickers[lastIndex].getImageUrl()
 
         val stickerPackBuilder = Indexables.stickerPackBuilder()
             .setName(STICKER_PACK_NAME)
@@ -89,82 +140,8 @@ object StickerIndexingUtil {
         return stickerPackBuilder.build()
     }
 
-    @Throws(IOException::class, FirebaseAppIndexingInvalidArgumentException::class)
-    private fun getIndexableStickers(context: Context): List<Indexable> {
-        val indexableStickers = arrayListOf<Indexable>()
-        val stickerBuilders = getStickerBuilders(context)
-
-        for (stickerBuilder in stickerBuilders) {
-            stickerBuilder
-                .setIsPartOf(
-                    Indexables.stickerPackBuilder()
-                        .setName(STICKER_PACK_NAME)
-                )
-                .put("keywords", "tag1", "tag2")
-            indexableStickers.add(stickerBuilder.build())
-        }
-
-        return indexableStickers
-    }
-
-    @Throws(IOException::class)
-    private fun getStickerBuilders(context: Context): List<StickerBuilder> {
-        val stickerBuilders = arrayListOf<StickerBuilder>()
-        val stickerColors = intArrayOf(Color.GREEN, Color.RED, Color.BLUE, Color.YELLOW, Color.MAGENTA)
-
-        val stickersDir = File(context.filesDir, "stickers")
-
-        if (!stickersDir.exists() && !stickersDir.mkdirs()) {
-            throw IOException("Stickers directory does not exist")
-        }
-
-        for (i in stickerColors.indices) {
-            val stickerFilename = getStickerFilename(i)
-            val stickerFile = File(stickersDir, stickerFilename)
-            val imageUrl = getStickerUrl(stickerFilename)
-            writeSolidColorBitmapToFile(stickerFile, stickerColors[i])
-
-            val stickerBuilder = Indexables.stickerBuilder()
-                .setName(getStickerFilename(i))
-                // Firebase App Indexing unique key that must match an intent-filter
-                // (e.g. mystickers://sticker/0)
-                .setUrl(String.format(STICKER_URL_PATTERN, i))
-                // http url or content uri that resolves to the sticker
-                // (e.g. http://www.google.com/sticker.png or content://some/path/0)
-                .setImage(imageUrl)
-                .setDescription("content description")
-                .setIsPartOf(
-                    Indexables.stickerPackBuilder()
-                        .setName(STICKER_PACK_NAME)
-                )
-                .put("keywords", "tag1", "tag2")
-            stickerBuilders.add(stickerBuilder)
-        }
-
-        return stickerBuilders
-    }
-
-    private fun getStickerFilename(index: Int) = String.format(STICKER_FILENAME_PATTERN, index)
-
-    private fun getStickerUrl(filename: String) = CONTENT_URI_ROOT + filename
-
-    /**
-     * Writes a simple bitmap to local storage. The image is a solid color with size 400x400
-     */
-    @Throws(IOException::class)
-    private fun writeSolidColorBitmapToFile(file: File, color: Int) {
-        if (!file.exists()) {
-            val bitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888)
-            bitmap.eraseColor(color)
-
-            var fos: FileOutputStream? = null
-            try {
-                fos = FileOutputStream(file)
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
-            } finally {
-                fos?.close()
-            }
-        }
+    private fun getStickerFilename(index: Int): String {
+        return HomeActivity.stickers[index].getName()
     }
 
 }
