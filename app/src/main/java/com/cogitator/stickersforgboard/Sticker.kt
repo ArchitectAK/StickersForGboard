997781872359e2264c92e25ecbbec67286464ccd
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-11-26 17:24:51 +0800

    added sticker model

---
+++ app/src/main/java/com/cogitator/stickersforgboard/Sticker.kt
@@ -0,0 +1,7 @@
+package com.cogitator.stickersforgboard
+
+/**
+ * @author Ankit Kumar on 26/11/2018
+ */
+
+class Sticker(val name: String, val imageResource: Int, val imageUrl: String, val keywords: Array<String>)
